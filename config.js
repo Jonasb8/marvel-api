f3c0f77e60415d346a24d87aa38fc2cd7d3a7ec4
Jonasb8 <bennisjonas@gmail.com>
2019-11-24 18:23:38 +0100

    retrieve list of heroes

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    marvel_api_key : 'f0842b20ab11ec38a1b1bfdfcbed1019',
+    marvel_api_secret : 'a4591d010f1a539bd606035deb1d195bbf348e60'
+}
--- routes/hero/index.js
+++ routes/hero/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../../config')
+const md5 = require('js-md5');
+const ts = 1;
+var request = require('request');
+
+const hash = md5(ts + config.marvel_api_secret + config.marvel_api_key)
 
 router.get('/', (req, res, next) => {
-    res.json({
-      api_ok:true
-    })
+    request(`https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${config.marvel_api_key}&hash=${hash}`, function (error, response, body) {
+        body = JSON.parse(body);
+        res.json({
+            characters : body.data.results
+        })
+    });
+
 })
 
 module.exports = router

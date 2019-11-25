4fe32dd9851dda1a3c98dc230a6ac9b03ade56fb
Jonasb8 <bennisjonas@gmail.com>
2019-11-25 10:02:20 +0100

    marvel api

--- routes/hero/index.js
+++ routes/hero/index.js
@@ -9,10 +9,19 @@ const hash = md5(ts + config.marvel_api_secret + config.marvel_api_key)
 
 router.get('/', (req, res, next) => {
     request(`https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${config.marvel_api_key}&hash=${hash}`, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         body = JSON.parse(body);
-        res.json({
-            characters : body.data.results
-        })
+        console.log('body:', body.data.results); // Print the HTML for the Google homepage.
+        if (error) {
+            res.json({
+                error
+            })
+        } else {
+          res.json({
+              characters : body.data.results
+          })
+        }
     });
 
 })
